f8dfb84036e44d7bc8d44471bea151ac9a6ff119
Furkan Kadıoğlu <furkan@castram.com>
2016-09-11 20:21:35 +0300

    Description, github url and author added

--- app/modules/CoxTable/Resources/views/default/main.blade.php
+++ app/modules/CoxTable/Resources/views/default/main.blade.php
@@ -3,10 +3,21 @@
 <title>Coxmaker @yield('title')</title>
 
 <!-- Begin: Styles -->
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Coxmaker is a simple tool designed to help shareholders determine their percentage distribution and ratio in an even manner.">
+<meta name="author" content="Rys">
+<link rel="icon" href="favicon.ico">
+
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
 <script src="<?php echo url('/assets/modules/coxtable/app.js'); ?>"></script>
+<script src="<?php echo url('/assets/modules/coxtable/angularbootstrap.js'); ?>"></script>
+
 
 <link rel="stylesheet" href="https://bootswatch.com/cerulean/bootstrap.min.css" />
 @yield('styles')
@@ -28,7 +39,7 @@
             
               <ul class="nav navbar-nav">
               <li><a href="<?php echo url('/'); ?>">Create</a></li>
-              <li><a href="<?php echo url('/github');  ?>">Github</a></li>
+              <li><a href="https://github.com/furkankadioglu/Coxmaker" target="_blank">Github</a></li>
               <li><a href="<?php echo url('CoxTable/642dc1cf3cc6717f44afc13a86196a75704205') ?>">Example CoxTable</a></li>
               </ul>
           </div>
@@ -49,7 +60,7 @@
 <footer>
 	<div class="container text-center">
 	<hr>
-		<small><i>Coxmaker - 2016</i></small>
+		<small><i><strong>Coxmaker</strong> - 2016</i></small>
 	</div>
 </footer>
 
